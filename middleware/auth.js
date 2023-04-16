45e6d8b2d7c9c7b23ac1793f8ac7a9efb45f6b17
Romain Cognard <romain@mynetwk.com>
2023-04-16 16:06:00 +0200

    Push

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,6 +1,9 @@
 const jwt = require('jsonwebtoken')
 
 module.exports = (req, res, next) => {
+
+    console.log(process.env.JSON_TOKEN)
+
     try {
         const token = req.headers.authorization.split(' ')[1]
         const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET')
