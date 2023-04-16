a73e597d3582882ee92a7edf2df374487ad3addd
Romain Cognard <romain@mynetwk.com>
2023-04-16 16:54:37 +0200

    Push

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const path = require('path')
 const cors = require('cors')
+const helmet = require('helmet')
 require('dotenv').config()
 
 const userRoutes = require('./routes/user')
@@ -9,15 +10,7 @@ const saucesRoutes = require('./routes/sauces')
 
 const app = express()
 app.use(express.json())
-
-
 app.use(cors())
-/*app.use((req, res, next)=>{
-    res.setHeader('Access-Control-Allow-Origin', '*')
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization')
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS')
-    next()
-})*/
 
 mongoose.set('strictQuery', false)
 mongoose.connect(
@@ -31,6 +24,8 @@ mongoose.connect(
 .catch(()=>console.log('Connexion à MongoDB échouée'))
 
 app.use('/images', express.static(path.join(__dirname, 'images')))
+
+app.use(helmet())
 app.use('/api/auth', userRoutes)
 app.use('/api/sauces', saucesRoutes)
 
