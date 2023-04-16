abab0c1f10b8b61b54666d4998e9ae34afa47906
Romain Cognard <romain@mynetwk.com>
2023-04-16 16:26:00 +0200

    Push

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const path = require('path')
+const cors = require('cors')
 require('dotenv').config()
 
 const userRoutes = require('./routes/user')
@@ -9,12 +10,14 @@ const saucesRoutes = require('./routes/sauces')
 const app = express()
 app.use(express.json())
 
-app.use((req, res, next)=>{
+
+app.use(cors())
+/*app.use((req, res, next)=>{
     res.setHeader('Access-Control-Allow-Origin', '*')
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization')
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS')
     next()
-})
+})*/
 
 mongoose.set('strictQuery', false)
 mongoose.connect(
